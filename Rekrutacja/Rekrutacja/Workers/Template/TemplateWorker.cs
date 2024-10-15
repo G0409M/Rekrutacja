83d0b009763bde9c5e464d4191551cd5bb9689fa
G0409M <gabrysia.maslanka@gmail.com>
2024-10-15 09:02:26 +0200

    Zadanie1

--- Rekrutacja/Rekrutacja/Workers/Template/TemplateWorker.cs
+++ Rekrutacja/Rekrutacja/Workers/Template/TemplateWorker.cs
@@ -11,13 +11,13 @@ using Rekrutacja.Workers.Template;
 using static Soneta.Ksiega.ZestawienieKS;
 
 //Rejetracja Workera - Pierwszy TypeOf określa jakiego typu ma być wyświetlany Worker, Drugi parametr wskazuje na jakim Typie obiektów będzie wyświetlany Worker
-[assembly: Worker(typeof(TemplateWorker), typeof(Pracownicy))]//stałe
+[assembly: Worker(typeof(TemplateWorker), typeof(Pracownicy))] 
 namespace Rekrutacja.Workers.Template
 {
     public class TemplateWorker
     {
         //Aby parametry działały prawidłowo dziedziczymy po klasie ContextBase
-        public class TemplateWorkerParametry : ContextBase//stałe
+        public class TemplateWorkerParametry : ContextBase 
         {
             [Caption("A")]
             public double A { get; set; }
@@ -25,65 +25,65 @@ namespace Rekrutacja.Workers.Template
             [Caption("B")]
             public double B { get; set; }
 
-            [Caption("Data obliczeń")]//stałe
-            public Date DataObliczen { get; set; }//stałe
+            [Caption("Data obliczeń")] 
+            public Date DataObliczen { get; set; } 
             [Caption("Operacja")]
             public string Operacja { get; set; }
-            public TemplateWorkerParametry(Context context) : base(context)//stałe
+            public TemplateWorkerParametry(Context context) : base(context) 
             {
                 this.A = 0;
                 this.B = 0;
                 this.Operacja = "+";
-                this.DataObliczen = Date.Today;//stałe
+                this.DataObliczen = Date.Today; 
             }
         }
         //Obiekt Context jest to pudełko które przechowuje Typy danych, aktualnie załadowane w aplikacji
         //Atrybut Context pobiera z "Contextu" obiekty które aktualnie widzimy na ekranie
         [Context]
-        public Context Cx { get; set; }//stałe
+        public Context Cx { get; set; } 
         //Pobieramy z Contextu parametry, jeżeli nie ma w Context Parametrów mechanizm sam utworzy nowy obiekt oraz wyświetli jego formatkę
         [Context]
-        public TemplateWorkerParametry Parametry { get; set; }//stałe
+        public TemplateWorkerParametry Parametry { get; set; } 
         //Atrybut Action - Wywołuje nam metodę która znajduje się poniżej
         [Action("Kalkulator",
            Description = "Prosty kalkulator ",
            Priority = 10,
            Mode = ActionMode.ReadOnlySession,
            Icon = ActionIcon.Accept,
-           Target = ActionTarget.ToolbarWithText)]//stałe
+           Target = ActionTarget.ToolbarWithText)] 
 
-        public void WykonajAkcje()//stałe
+        public void WykonajAkcje() 
         {
             //Włączenie Debug, aby działał należy wygenerować DLL w trybie DEBUG
-            DebuggerSession.MarkLineAsBreakPoint();//stałe
+            DebuggerSession.MarkLineAsBreakPoint(); 
             //Pobieranie danych z Contextu
-            Pracownik[] pracownicy = null;//stałe
-            if (this.Cx.Contains(typeof(Pracownik[])))//stałe
+            Pracownik[] pracownicy = null; 
+            if (this.Cx.Contains(typeof(Pracownik[]))) 
             {
-                pracownicy = (Pracownik[])this.Cx[typeof(Pracownik[])];//stałe
+                pracownicy = (Pracownik[])this.Cx[typeof(Pracownik[])]; 
             }
 
             //Modyfikacja danych
             //Aby modyfikować dane musimy mieć otwartą sesję, która nie jest read only
-            using (Session nowaSesja = this.Cx.Login.CreateSession(false, false, "ModyfikacjaPracownika"))//stałe
+            using (Session nowaSesja = this.Cx.Login.CreateSession(false, false, "ModyfikacjaPracownika")) 
             {
                 //Otwieramy Transaction aby można było edytować obiekt z sesji
-                using (ITransaction trans = nowaSesja.Logout(true))//stałe
+                using (ITransaction trans = nowaSesja.Logout(true)) 
                 {
                     foreach(Pracownik pracownik in pracownicy)
                     {
                         //Pobieramy obiekt z Nowo utworzonej sesji
-                        var pracownikZSesja = nowaSesja.Get(pracownik);//stałe
+                        var pracownikZSesja = nowaSesja.Get(pracownik); 
                          //Features - są to pola rozszerzające obiekty w bazie danych, dzięki czemu nie jestesmy ogarniczeni to kolumn jakie zostały utworzone przez producenta
-                        pracownikZSesja.Features["DataObliczen"] = this.Parametry.DataObliczen;//stałe
+                        pracownikZSesja.Features["DataObliczen"] = this.Parametry.DataObliczen; 
                         pracownikZSesja.Features["Wynik"] = Oblicz(this.Parametry.A, this.Parametry.B, this.Parametry.Operacja);
                         //Zatwierdzamy zmiany wykonane w sesji
-                        trans.CommitUI();//stałe
+                        trans.CommitUI(); 
                     }
                    
                 }
                 //Zapisujemy zmiany
-                nowaSesja.Save();//stałe
+                nowaSesja.Save(); 
             }
         }
         private double Oblicz(double a, double b, string operacja)
